92086c9bbf466f31495be6eab4fb1da0f11e062f
Hamdaan <mhamdaan26@gmail.com>
2023-06-24 14:10:09 +0400

    mmm

--- git hub demo upload/ContentView.swift
+++ git hub demo upload/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .foregroundColor(.brown)
     }
 }
 
@@ -24,3 +25,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
