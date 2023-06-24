b965944f4d5baba11896c88b050c881476f87a3b
Hamdaan <mhamdaan26@gmail.com>
2023-06-24 13:52:07 +0400

    Initial Commit

---
+++ git hub demo upload/git_hub_demo_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_hub_demo_uploadApp.swift
+//  git hub demo upload
+//
+//  Created by Hamdaann on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct git_hub_demo_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
